37e76363108c43f252056eaff5f3a44c44ca40dd
pugalDharundev <s10266942@connect.np.edu.sg>
2025-01-26 18:03:20 +0800

    Created 3 Classes

---
+++ Airline.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assg
+{
+    internal class Airline
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string code;
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        private Dictionary<String, Flight> flights;
+
+        public Dictionary<String, Flight> Flights
+        {
+            get { return flights; }
+            set { flights = value; }
+        }
+
+        public Airline() { }
+
+        public Airline(string name, string code, Dictionary<String, Flight> flights)
+        {
+            Name = name;
+            Code = code;
+            Flights = flights;
+        }
+
+        public void CalculateFees()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Airline: " + Name + " (" + Code + ")";
+        }
+
+        public void AddFlight(Flight flight)
+        {
+            Flights.Add(flight.FlightNumber, flight);
+        }
+
+        public void RemoveFlight(Flight flight)
+        {
+            Flights.Remove(flight.FlightNumber);
+        }
+
+    }
+}
